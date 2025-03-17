7727d984c00c8edd5d2f1b82d3f90178f3cd3272
prasetyo <prasetyo531@gmail.com>
2025-03-17 11:46:09 +0700

    add file to separate playwright config

---
+++ playwrightSetup.js
@@ -0,0 +1,39 @@
+// playwrightSetup.js
+const { defineConfig } = require("@playwright/test");
+
+export const config = defineConfig({
+  testDir: "./tests",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    baseURL: "http://localhost:3000",
+    browserName: "chromium",
+    headless: false,
+    screenshot: "on",
+    video: "retain-on-failure",
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "firefox",
+      use: { browserName: "firefox" },
+    },
+    {
+      name: "webkit",
+      use: { browserName: "webkit" },
+    },
+  ],
+});
